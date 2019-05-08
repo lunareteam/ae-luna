70d8577f42073bcb6e3ad32c556de3dbfc5ecf4d
jobucaldas <joaovictorbuenodecaldas@gmail.com>
2019-05-08 14:11:23 -0300

    Added comments

--- src/game/pong/ball.lua
+++ src/game/pong/ball.lua
@@ -1,6 +1,14 @@
+--[[Ball object]]--
+-- Makes itself an object --
 local ball= {}
-local name = require "game.pong.bar"
-function ball.initialize()
+
+-- Calls objects --
+local bar = require("game.pong.bar")
+
+-- Initializer function --
+function ball.initialize(barObj)
+  bar = barObj
+
   ball.x=400
   ball.y=300
   mod.vx=1
@@ -8,17 +16,22 @@ function ball.initialize()
   ball.vx=mod.vx
   ball.vy=mod.vy
   ball.r=10
-
 end
+
+-- Update function --
 function ball.update()
   ball.x=ball.x+ball.vx
   ball.y=ball.y+ball.vy
 end
+
+-- Drawer function --
 function ball.draw()
   love.graphics.circle("fill", ball.x, ball.y, ball.r, 30)
 end
+
+-- Cordenates function --
 function ball.coords()
-  ---vertical colision---
+  -- Vertical collision --
   if ball.y>=600-ball.r or ball.y<=0+ball.r then
     mod.y=-mod.y
     if ball.y>600 then
@@ -26,5 +39,6 @@ function ball.coords()
     else
       ball.y=ball.r
   end
-
 end
+
+return ball
--- src/game/pong/init.lua
+++ src/game/pong/init.lua
@@ -1,5 +1,6 @@
 local pong = {}
 local ball = require("game.pong.ball")
+local bar = require("game.pong.bar")
 
 function pong.initialize(screenObj, audioObj, inputObj, loaderObj, scene)
   -- Loads main objects --
@@ -8,10 +9,16 @@ function pong.initialize(screenObj, audioObj, inputObj, loaderObj, scene)
   input = inputObj
   loader = loaderObj
 
+  ball.initialize(bar)
+  bar.initialize()
 end
+
 function pong.draw()
 --{tudo o que você quiser colocar}
 end
+
 function pong.update()
 --{tudo o que você quiser colocar}
 end
+
+return pong
