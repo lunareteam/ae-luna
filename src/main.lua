eed36d0cd9e7348d692252ee3aee68f070ca5a55
João Bueno <joaovictorbuenodecaldas@gmail.com>
2019-05-22 16:58:32 -0300

    Changed some of the spacing

--- src/main.lua
+++ src/main.lua
@@ -1,16 +1,16 @@
 --[[This file calls the main objects]]--
 -- Creates objects --
 local loader = {}
-local input = {}
-local audio = {}
+local input  = {}
+local audio  = {}
 local screen = {}
 
 -- Main loader function --
 function love.load()
   -- Main objects and initialization --
   screen = require("objects.screen")
-  audio = require("objects.audio")
-  input = require("objects.input")
+  audio  = require("objects.audio")
+  input  = require("objects.input")
   loader = require("objects.loader")
   screen.initialize(loader, input)
   audio.initialize(loader)
